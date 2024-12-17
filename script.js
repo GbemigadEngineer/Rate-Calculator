1ab87e0f05b2512f44c4c0d27e7a7511a06db0b5
GbemigadEngineer <142888513+GbemigadEngineer@users.noreply.github.com>
2024-12-17 12:17:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 "use strict";
-const calculateMaturity = function () {
-  // get the input values from the form elements.
-  const principle = parseFloat(document.getElementById("principle").value);
 
-  const intrest = parseFloat(document.getElementById("intrestrate").value);
+const result = document.getElementById("result");
+const error = document.getElementById("error-message");
+const calculateMaturity =
+  // Attach event listner to calculate button
+  document
+    .getElementById("calculateBtn")
+    .addEventListener("click", function () {
+      // Prevent default action
+      // get the input values from the form elements.
+      const principle = parseFloat(document.getElementById("principle").value);
 
-  const tenure = parseFloat(document.getElementById("tenure").value);
+      const intrest = parseFloat(document.getElementById("intrestrate").value);
 
-  // Perform the calculation
+      const tenure = parseFloat(document.getElementById("tenure").value);
 
-  const maturityAmount = principle * (principle * intrest * tenure) / 100;
+      if (principle && intrest && tenure !== "") {
+        // Perform the calculation
 
-  // display the result
+        const maturityAmount =
+          (principle * (principle * intrest * tenure)) / 100;
 
-  document.getElementById(
-    "result"
-  ).innerText = `Maturity Amount : ${maturityAmount.toFixed(2)}`;
-};
-
-// Attach event listner to calculate button
-document 
-  .getElementById("calculateBtn")
-  .addEventListener("click", calculateMaturity);
+        if (!result.classList.contains("hidden")) {
+          result.innerText = `Maturity Amount : ${maturityAmount.toFixed(2)}`;
+        } else {
+          result.innerText = `Maturity Amount : ${maturityAmount.toFixed(2)}`;
+          result.classList.remove("hidden");
+          error.classList.add("hidden");
+        }
+      } else {
+        result.classList.add("hidden");
+        error.classList.remove("hidden");
+        error.style.textAlign = "center";
+        error.style.color = "red";
+      }
+    });
